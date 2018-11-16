44bca307d1cd66a53aff2edb728f764245e6bcd3
503-16 <a503-16@imac-3.local>
2018-11-16 13:01:23 +0900

    Initial Commit

---
+++ WebData1116/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebData1116
+//
+//  Created by 503-16 on 2018. 11. 16..
+//  Copyright © 2018년 the. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
